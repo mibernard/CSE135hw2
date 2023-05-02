3b1693cafb9ba6ef09e58d141de5691472c9b31b
Matthew Bernard <matthewbernard4@gmail.com>
2023-05-01 23:40:23 -0700

    adding source code

---
+++ source code/cgi-bin/php-session-1.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+​
+if (isset($_POST['name'])) {
+    $_SESSION['name'] = $_POST['name'];
+    $method = $_POST['method'];
+    if ($method == "php_cookies") {
+    header("Location: php-cookie-sessions-1.php");
+    } else if ($method == "php_url") {
+    $session_id = session_id();
+    header("Location: php-url-sessions-1.php" . session_name() . "=" . $session_id);
+    }
+} else {
+    header("Location: ../php-cgi-form.html");
+}
+?>
